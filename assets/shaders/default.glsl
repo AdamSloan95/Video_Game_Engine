b9d07eff0bbe7b97a6b1286cf4f97531b6de3364
Adam Sloan <28630579+AdamSloan95@users.noreply.github.comclear>
2024-02-12 00:37:11 +0000

    clear

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -1,25 +1,25 @@
-	#type vertex
-	#version 330 core 
+#type vertex
+#version 330 core 
 	
-	layout (location = 0) in vec3 aPos;
-	layout (location = 1) in vec4 aColour;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColour;
 	
-	out vec4 fColour;
+out vec4 fColour;
 	
-	void main()
-	{
-		fColour = aColour;
-		gl_Position = vec4(aPos,1.0);
-	}
+void main()
+{
+	fColour = aColour;
+	gl_Position = vec4(aPos,1.0);
+}
 
-	#type fragment
-	#version 330 core
+#type fragment
+#version 330 core
 
-	in vec4 fColour;
-	out vec4 colour;
+in vec4 fColour;
+out vec4 colour;
 
-	void main(){
-		colour = fColour;
-	}
+void main(){
+	colour = fColour;
+}
 	
 	
